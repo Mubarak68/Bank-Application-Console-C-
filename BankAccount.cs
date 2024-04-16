7c52a0e441190117eecc89f52b06f92a402b2708
KFH <KFH@LAPTOP-83314>
2024-04-16 08:16:06 +0300

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class BankAccount 
+{
+    private string name;
+    private int accountNumber;
+    private double balance;
+
+
+    public BankAccount(string name, int accountNumber, double initialBalance)
+    {
+        name = name;
+        accountNumber = accountNumber;
+        balance = initialBalance;
+    }
+
+    public void Deposit(double amount)
+    {
+        balance += amount;
+        Console.WriteLine($"\nDeposit of {amount} successful. New balance: {balance}");
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            Console.WriteLine($"\nWithdrawal of {amount} successful. New balance: {balance}");
+        }
+        else
+        {
+            Console.WriteLine("\nInsufficient funds. Withdrawal failed.");
+        }
+    }
+
+
+    public double GetBalance()
+    {
+        return balance;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Ahmad's Banking Application ");
+        Console.WriteLine();
+
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your account number: ");
+        int accountNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter your initial deposit: ");
+        double initialDeposit = double.Parse(Console.ReadLine());
+
+        BankAccount account = new BankAccount(name, accountNumber, initialDeposit);
+        Console.WriteLine("\nAccount setup successful!");
+
+        int choice;
+        do
+        {
+            Console.WriteLine("\nMain Menu:");
+            Console.WriteLine("1. Deposit ");
+            Console.WriteLine("2. Withdraw ");
+            Console.WriteLine("3. View Balance ");
+            Console.WriteLine("4. Exit ");
+            Console.Write("Please select a number: ");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("\nEnter deposit amount: ");
+                    double depositAmount = double.Parse(Console.ReadLine());
+                    account.Deposit(depositAmount);
+                    break;
+                case 2:
+                    Console.Write("\nEnter withdrawal amount: ");
+                    double withdrawalAmount = double.Parse(Console.ReadLine());
+                    account.Withdraw(withdrawalAmount);
+                    break;
+                case 3:
+                    Console.WriteLine($"\nYour current balance is: {account.GetBalance()}");
+                    break;
+                case 4:
+                    Console.WriteLine("\nThank you for using the Basic Banking Application. Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("\nInvalid choice. Please try again.");
+                    break;
+            }
+        } while (choice != 4);
+    }
+}
